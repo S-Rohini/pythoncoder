ac3ba301e83546d8e5f442dfe5013c2f62368545
S-Rohini <84655753+S-Rohini@users.noreply.github.com>
2021-05-23 12:15:30 +0530

    Create radius.py
    This file radius of circle and filename from the user on screen

--- Radius.py
+++ Radius.py
@@ -1,3 +1,7 @@
-From math import pi
+1.from math import pi
 r=float (int("input the radius of the circle:"))
 Print ("the area of the circle with radius+str(r)+"is:"+str(pi*r**2))
+
+2.filename=int("input the filename:")
+ f_extns=filename.spilt(".")
+Print ("the extension of the file is:"+repr(f_extns[-1]))
